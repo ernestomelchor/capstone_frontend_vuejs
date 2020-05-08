c0ff4a63bbe52a11266b9f81316066919f7fb7cc
ernestomelchor <nurturelove88@gmail.com>
2020-05-08 15:47:12 -0500

    add GamesIndex static page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import GamesIndex from "../views/GamesIndex.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/games",
+    name: "games-index",
+    component: GamesIndex,
   },
 ];
 
