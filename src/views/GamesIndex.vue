9dcf6dac24966b7450cfeb8cb4791e459fdb40a6
ernestomelchor <nurturelove88@gmail.com>
2020-05-08 16:06:18 -0500

    add ability to see all games connecting backend to frontend via axios

--- src/views/GamesIndex.vue
+++ src/views/GamesIndex.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="games-index">
     <h1>{{ message }}</h1>
+    <h1>{{ games }}</h1>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
       message: "The Beautiful Game!",
+      games: [],
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/games").then((response) => {
+      this.games = response.data;
+    });
+  },
   methods: {},
 };
 </script>
