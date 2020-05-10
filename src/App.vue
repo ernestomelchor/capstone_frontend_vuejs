beed4b9a45803d925cc717406a7344fca8c99b32
ernestomelchor <nurturelove88@gmail.com>
2020-05-09 20:52:03 -0500

    add GamesIndex action that is user friendly, add static join button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/games">All Games</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/games">All Games</router-link>|
+      <router-link to="/about">About</router-link>|
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
+img {
+  max-width: 400px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/GamesIndex.vue
+++ src/views/GamesIndex.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="games-index">
     <h1>{{ message }}</h1>
-    <h1>{{ games }}</h1>
+    <div v-for="game in games">
+      <img :src="`${game.field.image_url}`" alt="Picture of Soccer Field" />
+      <p>
+        <b>When:</b>
+        {{ game.scheduled }}
+      </p>
+      <p>
+        <b>Where:</b>
+        {{ game.field.name }}
+      </p>
+      <p>
+        <b>Address:</b>
+        {{ game.field.address }}
+      </p>
+      <p>
+        <b>Number of People Attending So Far:</b>
+        {{ game.players_attending }}
+      </p>
+      <!-- Insert a Join Button Here -->
+      <div>
+        <button>Show Up to This Game</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,14 +36,14 @@ export default {
   data: function() {
     return {
       message: "The Beautiful Game!",
-      games: [],
+      games: []
     };
   },
   created: function() {
-    axios.get("/api/games").then((response) => {
+    axios.get("/api/games").then(response => {
       this.games = response.data;
     });
   },
-  methods: {},
+  methods: {}
 };
 </script>
