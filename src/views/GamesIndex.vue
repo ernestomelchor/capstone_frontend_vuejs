ea6383a9ccf165f125d253b43ead1dea5ab7e1a7
ernestomelchor <nurturelove88@gmail.com>
2020-05-16 00:59:08 -0500

    add map display using mapxbox api and can show a random marker on the map (Chicago, IL)

--- src/views/GamesIndex.vue
+++ src/views/GamesIndex.vue
@@ -2,6 +2,9 @@
   <div class="games-index text-white text-center">
     <h1>{{ message }}</h1>
     <br />
+    <div id="map"></div>
+    <br />
+    <br />
     <div v-for="game in games">
       <div v-for="player in game.player_games">
         <!-- <b>{{ player.player_game_id }}</b> -->
@@ -44,9 +47,32 @@
 <style></style>
 
 <script>
+// @ is an alias to /src
 import axios from "axios";
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  },
+  mounted: function() {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiZXJuZXN0b21lbGNob3IiLCJhIjoiY2s5YnpvcmljMDAwYzNrbWxiaDM1NjZ4bCJ9.xg_ekA9tmYEZcUDVqBFlOQ";
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: [-87.6298, 41.8781], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      "Construction on the Washington Monument began in 1848."
+    );
+    var marker = new mapboxgl.Marker()
+      .setLngLat([-87.6298, 41.8781])
+      .setPopup(popup)
+      .addTo(map);
+  },
   data: function() {
     return {
       message: "Come Join a Game!",
