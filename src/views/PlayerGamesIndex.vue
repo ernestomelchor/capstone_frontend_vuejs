2d52ebc7c1fcfd9236af1dfb2e46a19fd2392e0f
ernestomelchor <nurturelove88@gmail.com>
2020-05-20 12:23:36 -0500

    configure image sizes so they do not shrink when you go to mobile version

--- src/views/PlayerGamesIndex.vue
+++ src/views/PlayerGamesIndex.vue
@@ -34,7 +34,6 @@
                   </div>
                 </div>
                 <img
-                  class="img-fluid"
                   :src="`${playerGame.field_image}`"
                   alt="Picture of Soccer Field You Are Attending"
                 />
